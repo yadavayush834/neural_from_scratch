105d5688169e8c33bfabdec98ba5c1fa97293ea9
yadavayush834 <yadavayush834@users.noreply.github.com>
2026-08-30 16:22:03 +0530

    Add neural network implementation

---
+++ neuron.py
@@ -0,0 +1,20 @@
+def neuron(input,weights,bias):
+    total = 0 
+
+
+    for i in range(len(input)):
+        total += weights[i] * input[i]
+
+
+    total += bias
+    return total 
+
+input = [2,4]
+weights = [0.5,-0.2]
+
+bias = 0.1 
+
+output = neuron(input ,weights,bias)
+print(output)
+
+
